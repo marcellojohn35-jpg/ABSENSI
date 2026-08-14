d272490cd3f66c95a7758c2467fab7b186e52839
marcellojohn35-jpg <marcellojohn35@gmail.com>
2026-08-14 13:39:51 +0700

    Fix CSV export formatting

--- script.js
+++ script.js
@@ -418,7 +418,8 @@ function exportToCSV() {
         csv.push(rowData.join(','));
     });
 
-    const blob = new Blob([csv.join('\n')], { type: 'text/csv' });
+    const csvContent = '\uFEFF' + csv.join('\r\n');
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
     link.download = `absensi_${getJakartaDateStr()}.csv`;
